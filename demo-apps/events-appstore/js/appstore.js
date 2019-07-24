7df9e4d3974f992ef283961f0397d6bddea0d872
Brian Rogers <b.c.rogers@swansea.ac.uk>
2019-07-24 15:58:58 +0100

    Swiper modifications for swansea events app

---
+++ demo-apps/events-appstore/js/appstore.js
@@ -0,0 +1,14 @@
+$(function(){
+	//Featured Slide
+	var featuredSwiper = $('.featured').swiper({
+		slidesPerView: 'auto',
+		centeredSlides: true,
+		initialSlide:2,
+		keyboardControl: true,
+		tdFlow: {
+			rotate : 50,
+			stretch :0,
+			depth: 400
+		}
+	})
+})
